7a72399f74a0d5c199d26b9614e41ec3984080e7
FlutLab.io <>
2022-03-10 17:41:52 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/capitalappapp/generatedcoverwidget1/GeneratedCoverWidget1.dart';
+import 'package:flutterapp/capitalappapp/generatedsplashwidget/GeneratedSplashWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedsignupwidget/GeneratedSignUpWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedloginwidget/GeneratedLoginWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedvefificationcodewidget/GeneratedVefificationCodeWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedressetpasswordwidget/GeneratedRessetPasswordWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedphoneregistrationwidget/GeneratedPhoneRegistrationWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedphoneregistrationwidget1/GeneratedPhoneRegistrationWidget1.dart';
+import 'package:flutterapp/capitalappapp/generateddoruakwidget1/GeneratedDoruakWidget1.dart';
+import 'package:flutterapp/capitalappapp/generatedsendviiiburgeriwidget/GeneratedSendviiiBurgeriWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedalacartewidget3/GeneratedAlaCarteWidget3.dart';
+import 'package:flutterapp/capitalappapp/generatedorbeisupewidget/GeneratedorbeiSupeWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedsalatewidget5/GeneratedSalateWidget5.dart';
+import 'package:flutterapp/capitalappapp/generatedlahmadunwidget6/GeneratedLahmadunWidget6.dart';
+import 'package:flutterapp/capitalappapp/generatedpidewidget7/GeneratedPideWidget7.dart';
+import 'package:flutterapp/capitalappapp/generatedspecialpizzawidget/GeneratedSpecialPizzaWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedpastairiotowidget/GeneratedPastaiRiotoWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedcbrspecialwidget1/GeneratedCBRSpecialWidget1.dart';
+import 'package:flutterapp/capitalappapp/generatedkebabsendviwidget2/GeneratedKebabSendviWidget2.dart';
+import 'package:flutterapp/capitalappapp/generatedcapitaltostwidget2/GeneratedCapitalTostWidget2.dart';
+import 'package:flutterapp/capitalappapp/generatedadanakebabwidget2/GeneratedAdanaKebabWidget2.dart';
+import 'package:flutterapp/capitalappapp/generatedomletsolowidget1/GeneratedOmletSoloWidget1.dart';
+import 'package:flutterapp/capitalappapp/generatedrozbifsendviwidget1/GeneratedRozbifSendviWidget1.dart';
+import 'package:flutterapp/capitalappapp/generatedparadajzsupawidget1/GeneratedParadajzSupaWidget1.dart';
+import 'package:flutterapp/capitalappapp/generatedpileasusamsalatawidget1/GeneratedPileaSusamSalataWidget1.dart';
+import 'package:flutterapp/capitalappapp/generatedlahmadunwidget13/GeneratedLahmadunWidget13.dart';
+import 'package:flutterapp/capitalappapp/generatedpovrtnafantazijawidget1/GeneratedPovrtnaFantazijaWidget1.dart';
+import 'package:flutterapp/capitalappapp/generatedpastauparadajzsosuwidget1/GeneratedPastauparadajzsosuWidget1.dart';
+import 'package:flutterapp/capitalappapp/generatedpileifilesagrilawidget1/GeneratedPileifilesagrilaWidget1.dart';
+import 'package:flutterapp/capitalappapp/generatedpidewidget12/GeneratedPideWidget12.dart';
+import 'package:flutterapp/capitalappapp/generatedkorpawidget11/GeneratedKorpaWidget11.dart';
+import 'package:flutterapp/capitalappapp/generatedreviewrestoranwidget/GeneratedReviewRestoranWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedkontaktwidget/GeneratedKontaktWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedplaanjewidget/GeneratedPlaanjeWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedrezervacijawidget/GeneratedRezervacijaWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedreviewswidget/GeneratedReviewsWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedratingwidget/GeneratedRatingWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedaddnewaddresswidget/GeneratedAddnewaddressWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedprofilewidget/GeneratedProfileWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedizaberitejezikwidget/GeneratedIzaberitejezikWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedmaleuserwidget/GeneratedMaleUserWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedframe11widget/GeneratedFrame11Widget.dart';
+import 'package:flutterapp/capitalappapp/generatedcomponent2widget1/GeneratedComponent2Widget1.dart';
+import 'package:flutterapp/capitalappapp/generatedomiljenahranawidget/GeneratedOmiljenahranaWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedstyleguidewidget1/GeneratedStyleGuideWidget1.dart';
+import 'package:flutterapp/capitalappapp/generatedgroup17963widget2/GeneratedGroup17963Widget2.dart';
+import 'package:flutterapp/capitalappapp/generatedrectangle139widget14/GeneratedRectangle139Widget14.dart';
+import 'package:flutterapp/capitalappapp/generatedsignupwidget/GeneratedSIGNUPWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedframe4widget/GeneratedFrame4Widget.dart';
+import 'package:flutterapp/capitalappapp/generatedrectangle4322widget/GeneratedRectangle4322Widget.dart';
+import 'package:flutterapp/capitalappapp/generatedrectangle4325widget/GeneratedRectangle4325Widget.dart';
+import 'package:flutterapp/capitalappapp/generatedgroup17860widget12/GeneratedGroup17860Widget12.dart';
+import 'package:flutterapp/capitalappapp/generatedrectangle4151widget1/GeneratedRectangle4151Widget1.dart';
+import 'package:flutterapp/capitalappapp/generatedupcomingwidget/GeneratedUpcomingWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedrectangle4324widget/GeneratedRectangle4324Widget.dart';
+import 'package:flutterapp/capitalappapp/generatedtrackorderwidget/GeneratedTrackOrderWidget.dart';
+import 'package:flutterapp/capitalappapp/generatedframe6widget2/GeneratedFrame6Widget2.dart';
+import 'package:flutterapp/capitalappapp/generatedcomponent1widget/GeneratedComponent1Widget.dart';
+import 'package:flutterapp/capitalappapp/generatedcomponent1widget1/GeneratedComponent1Widget1.dart';
+
+void main() {
+  runApp(CapitalAppApp());
+}
+
+class CapitalAppApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedCoverWidget1',
+      routes: {
+        '/GeneratedCoverWidget1': (context) => GeneratedCoverWidget1(),
+        '/GeneratedSplashWidget': (context) => GeneratedSplashWidget(),
+        '/GeneratedSignUpWidget': (context) => GeneratedSignUpWidget(),
+        '/GeneratedLoginWidget': (context) => GeneratedLoginWidget(),
+        '/GeneratedVefificationCodeWidget': (context) => GeneratedVefificationCodeWidget(),
+        '/GeneratedRessetPasswordWidget': (context) => GeneratedRessetPasswordWidget(),
+        '/GeneratedPhoneRegistrationWidget': (context) => GeneratedPhoneRegistrationWidget(),
+        '/GeneratedPhoneRegistrationWidget1': (context) => GeneratedPhoneRegistrationWidget1(),
+        '/GeneratedDoruakWidget1': (context) => GeneratedDoruakWidget1(),
+        '/GeneratedSendviiiBurgeriWidget': (context) => GeneratedSendviiiBurgeriWidget(),
+        '/GeneratedAlaCarteWidget3': (context) => GeneratedAlaCarteWidget3(),
+        '/GeneratedorbeiSupeWidget': (context) => GeneratedorbeiSupeWidget(),
+        '/GeneratedSalateWidget5': (context) => GeneratedSalateWidget5(),
+        '/GeneratedLahmadunWidget6': (context) => GeneratedLahmadunWidget6(),
+        '/GeneratedPideWidget7': (context) => GeneratedPideWidget7(),
+        '/GeneratedSpecialPizzaWidget': (context) => GeneratedSpecialPizzaWidget(),
+        '/GeneratedPastaiRiotoWidget': (context) => GeneratedPastaiRiotoWidget(),
+        '/GeneratedCBRSpecialWidget1': (context) => GeneratedCBRSpecialWidget1(),
+        '/GeneratedKebabSendviWidget2': (context) => GeneratedKebabSendviWidget2(),
+        '/GeneratedCapitalTostWidget2': (context) => GeneratedCapitalTostWidget2(),
+        '/GeneratedAdanaKebabWidget2': (context) => GeneratedAdanaKebabWidget2(),
+        '/GeneratedOmletSoloWidget1': (context) => GeneratedOmletSoloWidget1(),
+        '/GeneratedRozbifSendviWidget1': (context) => GeneratedRozbifSendviWidget1(),
+        '/GeneratedParadajzSupaWidget1': (context) => GeneratedParadajzSupaWidget1(),
+        '/GeneratedPileaSusamSalataWidget1': (context) => GeneratedPileaSusamSalataWidget1(),
+        '/GeneratedLahmadunWidget13': (context) => GeneratedLahmadunWidget13(),
+        '/GeneratedPovrtnaFantazijaWidget1': (context) => GeneratedPovrtnaFantazijaWidget1(),
+        '/GeneratedPastauparadajzsosuWidget1': (context) => GeneratedPastauparadajzsosuWidget1(),
+        '/GeneratedPileifilesagrilaWidget1': (context) => GeneratedPileifilesagrilaWidget1(),
+        '/GeneratedPideWidget12': (context) => GeneratedPideWidget12(),
+        '/GeneratedKorpaWidget11': (context) => GeneratedKorpaWidget11(),
+        '/GeneratedReviewRestoranWidget': (context) => GeneratedReviewRestoranWidget(),
+        '/GeneratedKontaktWidget': (context) => GeneratedKontaktWidget(),
+        '/GeneratedPlaanjeWidget': (context) => GeneratedPlaanjeWidget(),
+        '/GeneratedRezervacijaWidget': (context) => GeneratedRezervacijaWidget(),
+        '/GeneratedReviewsWidget': (context) => GeneratedReviewsWidget(),
+        '/GeneratedRatingWidget': (context) => GeneratedRatingWidget(),
+        '/GeneratedAddnewaddressWidget': (context) => GeneratedAddnewaddressWidget(),
+        '/GeneratedProfileWidget': (context) => GeneratedProfileWidget(),
+        '/GeneratedIzaberitejezikWidget': (context) => GeneratedIzaberitejezikWidget(),
+        '/GeneratedMaleUserWidget': (context) => GeneratedMaleUserWidget(),
+        '/GeneratedFrame11Widget': (context) => GeneratedFrame11Widget(),
+        '/GeneratedComponent2Widget1': (context) => GeneratedComponent2Widget1(),
+        '/GeneratedOmiljenahranaWidget': (context) => GeneratedOmiljenahranaWidget(),
+        '/GeneratedStyleGuideWidget1': (context) => GeneratedStyleGuideWidget1(),
+        '/GeneratedGroup17963Widget2': (context) => GeneratedGroup17963Widget2(),
+        '/GeneratedRectangle139Widget14': (context) => GeneratedRectangle139Widget14(),
+        '/GeneratedFrame4Widget': (context) => GeneratedFrame4Widget(),
+        '/GeneratedRectangle4322Widget': (context) => GeneratedRectangle4322Widget(),
+        '/GeneratedRectangle4325Widget': (context) => GeneratedRectangle4325Widget(),
+        '/GeneratedGroup17860Widget12': (context) => GeneratedGroup17860Widget12(),
+        '/GeneratedRectangle4151Widget1': (context) => GeneratedRectangle4151Widget1(),
+        '/GeneratedUpcomingWidget': (context) => GeneratedUpcomingWidget(),
+        '/GeneratedRectangle4324Widget': (context) => GeneratedRectangle4324Widget(),
+        '/GeneratedTrackOrderWidget': (context) => GeneratedTrackOrderWidget(),
+        '/GeneratedFrame6Widget2': (context) => GeneratedFrame6Widget2(),
+        '/GeneratedComponent1Widget': (context) => GeneratedComponent1Widget(),
+        '/GeneratedComponent1Widget1': (context) => GeneratedComponent1Widget1(),
+      },
+    );
+  }
+}
